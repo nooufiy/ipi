d536d065d103275e4e9e8f6e1a376490cb823ff1
nou <83199729+nooufiy@users.noreply.github.com>
2021-05-20 15:32:55 +0700

    Create rmote.sh

---
+++ rmote.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+curl -s https://raw.githubusercontent.com/nooufiy/ipi/main/run.sh >tmp.sh
+bash tmp.sh
+
+
+# http://stackoverflow.com/questions/5735666/execute-bash-script-from-url
+bash <(curl -s https://raw.githubusercontent.com/nooufiy/ipi/main/run.sh)
+
+# http://stackoverflow.com/questions/4642915/passing-parameters-to-bash-when-executing-a-script-fetched-by-curl
+curl https://raw.githubusercontent.com/nooufiy/ipi/main/run.sh | bash -s arg1 arg2
