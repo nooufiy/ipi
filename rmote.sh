b28cbfcf07d0d2e549d47951c83a5faba2e80ff7
nou <83199729+nooufiy@users.noreply.github.com>
2021-05-20 19:07:49 +0700

    kasiopsen

--- rmote.sh
+++ rmote.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+# [1] file
 curl -s https://raw.githubusercontent.com/nooufiy/ipi/main/run.sh >tmp.sh
 bash tmp.sh
 
-
+# [2] non par
 # http://stackoverflow.com/questions/5735666/execute-bash-script-from-url
 bash <(curl -s https://raw.githubusercontent.com/nooufiy/ipi/main/run.sh)
 bash <(curl -s https://raw.githubusercontent.com/nooufiy/sta/main/insta.sh)
 
+# [3] with par
 # http://stackoverflow.com/questions/4642915/passing-parameters-to-bash-when-executing-a-script-fetched-by-curl
 curl https://raw.githubusercontent.com/nooufiy/ipi/main/run.sh | bash -s arg1 arg2
+curl https://raw.githubusercontent.com/nooufiy/ipi/main/run.sh | bash -s ilm
+curl https://raw.githubusercontent.com/nooufiy/ipi/main/run.sh | bash -s fat 123.45
