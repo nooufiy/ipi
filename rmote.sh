ca8cd934b485ba5afa544505e07171e1470953a1
nou <83199729+nooufiy@users.noreply.github.com>
2021-06-17 16:24:09 +0700

    Update rmote.sh

--- rmote.sh
+++ rmote.sh
@@ -8,6 +8,7 @@ bash tmp.sh
 # http://stackoverflow.com/questions/5735666/execute-bash-script-from-url
 bash <(curl -s https://raw.githubusercontent.com/nooufiy/ipi/main/run.sh)
 bash <(curl -s https://raw.githubusercontent.com/nooufiy/sta/main/insta.sh)
+bash <(curl -s https://raw.githubusercontent.com/nooufiy/pndh/main/gensenssh.sh)
 
 # [3] with par
 # http://stackoverflow.com/questions/4642915/passing-parameters-to-bash-when-executing-a-script-fetched-by-curl
