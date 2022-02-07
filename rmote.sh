ca59066a436e7d882fc70574890ca492a9748cd5
noou <83199729+nooufiy@users.noreply.github.com>
2022-02-08 03:02:07 +0700

    Update rmote.sh

--- rmote.sh
+++ rmote.sh
@@ -19,3 +19,9 @@ curl https://raw.githubusercontent.com/nooufiy/ipi/main/run.sh | bash -s fat 134
 
 curl https://raw.githubusercontent.com/nooufiy/gnod/main/ins.sh | bash -s nginx n
 rsync -avzh --progress root@104.248.193.237:/home/* /home
+curl https://raw.githubusercontent.com/nooufiy/gnod/main/runidxnod.sh | bash
+
+# Setatik [add volume 120 di fold /mnt]
+curl https://raw.githubusercontent.com/nooufiy/ipi/main/run.sh | bash -s ilm
+rsync -avzh --progress root@167.71.51.241:/mnt/* /mnt
+#auto: curl https://raw.githubusercontent.com/nooufiy/ipi/main/statik.sh | bash
