520e03f57770d718288a547bcbf993bf3c9197d0
noou <83199729+nooufiy@users.noreply.github.com>
2022-03-06 23:52:33 +0700

    Update statik.sh

--- statik.sh
+++ statik.sh
@@ -4,5 +4,5 @@ ip=$1
 
 # Setatik [add volume 120 di fold /mnt]
 curl https://raw.githubusercontent.com/nooufiy/ipi/main/run.sh | bash -s ilm
-rsync -avzh --progress root@"$ip":/mnt/* /mnt
+rsync -avzh --progress root@"$ip":/mnt/* /mnt/dataku
 
