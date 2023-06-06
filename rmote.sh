b050f1cccb7b4bf93bd7cd7989f12d29dae83c12
noou <83199729+nooufiy@users.noreply.github.com>
2023-06-07 06:37:17 +0700

    Update rmote.sh

--- rmote.sh
+++ rmote.sh
@@ -19,6 +19,7 @@ curl https://raw.githubusercontent.com/nooufiy/ipi/main/run.sh | bash -s fat 134
 
 https://github.com/nooufiy/ilamp74/blob/main/ins.sh
 curl https://raw.githubusercontent.com/nooufiy/ilamp74/main/ins.sh | bash
+curl https://raw.githubusercontent.com/nooufiy/ilamp74/main/ins-db.sh | bash
 curl https://raw.githubusercontent.com/nooufiy/ilamp74/main/ins-c8.sh | bash
 curl https://raw.githubusercontent.com/nooufiy/ilamp81/main/ins-c7.sh | bash
 
